1b3206ec9f941be2ea6bdcbe7f619808b945ee60
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-02-20 14:25:08 +0800

    2024.2.20

--- seqkit.sh
+++
@@ -1,5 +0,0 @@
-# move chr1A to certain file 
-cat iwgsc_refseqv1.0_TransposableElements_2017Mar13.gff3 | grep -w 'chr1A' > chr1A/chr1A.gff3
-seqkit grep -p 1 abd_iwgscV1.fa -o chr1A/chr1A1.fa
-
-seqkit stats 
