68f1a93dd31091e09e42c336d75ea8451614bd3c
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-02-21 19:44:26 +0800

    cut

--- 02TEdetect/abd_data_process/cut_chr1A.sh
+++ 02TEdetect/abd_data_process/cut_chr1A.sh
@@ -7,4 +7,4 @@ echo "1"
 twoBitInfo -nBed genome.2bit N.bed
 echo "2"
 awk '{print $1 "\t" $2}' $1.fai > $1.genome
-bedtools complement -i N.bed -g $1.genome | bedtools getfasta -fo genome.temp -fi $1 -bed -
+bedtools complement -i N.bed -g $1.genome | bedtools getfasta -fo chr1AN.fa -fi $1 -bed -
