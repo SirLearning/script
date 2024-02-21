104cc24dbd799f7b140ad2ba5683daf1ffc4f00e
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-02-21 19:50:17 +0800

    cut

--- 02TEdetect/abd_data_process/cut_chr1A.sh
+++ 02TEdetect/abd_data_process/cut_chr1A.sh
@@ -1,10 +1,11 @@
-# test 2
-# cat chr1A.fa | seqkit subseq -r 1:1000000 > chr1A1M.fa
+# generate chr1A1M.fa
+cat $1 | seqkit subseq -r 1:1000000 > chr1A1M.fa
 
-# cut fasta into different length
+# cut fasta into different length, generate chr1AN.fa
 faToTwoBit $1 genome.2bit
 echo "1"
 twoBitInfo -nBed genome.2bit N.bed
 echo "2"
+samtools faidx $1
 awk '{print $1 "\t" $2}' $1.fai > $1.genome
 bedtools complement -i N.bed -g $1.genome | bedtools getfasta -fo chr1AN.fa -fi $1 -bed -
