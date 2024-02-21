632e5ed640c664dba980be8efdab98c15f63d7ed
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-02-21 21:31:31 +0800

    cut

--- 02TEdetect/abd_data_process/chr1AN_name_in_number.py
+++ 02TEdetect/abd_data_process/chr1AN_name_in_number.py
@@ -2,6 +2,6 @@ with open('chr1AN.fa', 'r') as infile, open('chr1AN_modified.fa', 'w') as outfil
     line_number = 1
     for line in infile:
         if line.startswith('>'):
-            line = line.replace(':\w+-\w+', f':{line_number}')
+            line = line.replace(':\d+-\d+', f':{line_number}')
             line_number += 1
         outfile.write(line)
