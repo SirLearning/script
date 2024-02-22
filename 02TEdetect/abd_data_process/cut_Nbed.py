c21001b173b716c9a4abcf15dc0214ceb6f1a55f
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-02-22 20:44:09 +0800

    1

--- 02TEdetect/abd_data_process/cut_Nbed.py
+++ 02TEdetect/abd_data_process/cut_Nbed.py
@@ -1,32 +1,3 @@
-# import sys
-#
-# input_filename = 'N.bed' # sys.argv[1]
-# N_length = 1000 # int(sys.argv[2])
-#
-# with open(input_filename) as f:
-#     elements = {}
-#     chr0 = ''
-#     count = 0
-#     for line in f:
-#         element = line.split('\t')
-#         chrom = element[0]
-#         start = int(element[1])
-#         end = int(element[2])
-#         numN = end - start - 1
-#         if numN < N_length:
-#             continue
-#         count += 1
-#         elements[chrom] = []
-#         elements[chrom].append((start, end, numN))
-#         if chr0 == '':
-#             frag = start
-#         else:
-#             frag = start - elements[chr0][1]
-#         elements[chrom].append(frag)
-#         chr0 = chrom
-# for chrom in elements:
-#     print(chrom, elements[chrom], sep='\t')
-
 import sys
 
 input_filename = sys.argv[1]
@@ -62,3 +33,32 @@ print("Average frag:", average_frag)
 with open(output_filename, 'w') as f:
     for key in elements:
         f.write(f"{elements[key]['chrom']}\t{elements[key]['start']}\t{elements[key]['end']}\n")
+
+# import sys
+#
+# input_filename = 'N.bed' # sys.argv[1]
+# N_length = 1000 # int(sys.argv[2])
+#
+# with open(input_filename) as f:
+#     elements = {}
+#     chr0 = ''
+#     count = 0
+#     for line in f:
+#         element = line.split('\t')
+#         chrom = element[0]
+#         start = int(element[1])
+#         end = int(element[2])
+#         numN = end - start - 1
+#         if numN < N_length:
+#             continue
+#         count += 1
+#         elements[chrom] = []
+#         elements[chrom].append((start, end, numN))
+#         if chr0 == '':
+#             frag = start
+#         else:
+#             frag = start - elements[chr0][1]
+#         elements[chrom].append(frag)
+#         chr0 = chrom
+# for chrom in elements:
+#     print(chrom, elements[chrom], sep='\t')
