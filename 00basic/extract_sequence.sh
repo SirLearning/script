015b5e6015171378ebda9b7b96e3440d496525e0
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-09 23:02:28 +0800

    1

--- 00basic/extract_sequence.sh
+++ 00basic/extract_sequence.sh
@@ -1,7 +1,7 @@
 # move chr1A to certain file 
 cat iwgsc_refseqv1.0_TransposableElements_2017Mar13.gff3 | grep -w 'chr1A' > chr1A/chr1A.gff3
 seqkit grep -p 1 abd_iwgscV1.fa -o chr1A/chr1A1.fa
-
+seqkit grep -f chr1B.name -o chr1B.fa abd_iwgscV1.fa
 seqkit stats 
 
 # change .gff file to .bed file by awk
