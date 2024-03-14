44a6f947e92fd29d9eacfe568d99812dde542faa
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-14 16:24:28 +0800

    Update 03intactLTR.sh

--- 04analysis/01Rstats/03intactLTR.sh
+++ 04analysis/01Rstats/03intactLTR.sh
@@ -3,7 +3,7 @@ awk '$3~"LTR_retrotransposon"' \
   | grep "Method=structural" > intact.LTR.gff3
 
 # 有strand信息的LTR 23.EDTA/Aly/genome.fa.mod.EDTA.TEanno.gff3
-awk -F';' '{print $1' intact.LTR.gff3\
+awk -F';' '{print $1}' intact.LTR.gff3\
   | sed 's/ID=//' | awk '$7="?"' \
   | awk '{print $1"\t"$4-1"\t"$5"\t"$9"\t.\t"$7}' >> intact.LTR.bed6
 
