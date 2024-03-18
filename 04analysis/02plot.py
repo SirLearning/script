d5ef77203187329140bb5f9d0c8be3b03cc40767
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-18 17:10:27 +0800

    cut

--- 04analysis/02plot.py
+++ 04analysis/02plot.py
@@ -2,6 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+chr1A_allTE_summ = pd.read_table('data/01chr1A.anno.stats', sep='\s+', header=0, index_col=0)
+chr1A_allTE_summ.index.name = 'Classification'
+
+print(chr1A_allTE_summ.head())
+
 # 1. plot length
 plt.figure(figsize=(10, 10))
 sns.barplot(data=chr1A_allTE_summ, x='Classification', y='size')
