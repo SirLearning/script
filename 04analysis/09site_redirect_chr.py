1773ed410f4aa6cbd610aa5bfce48ea4b7cdfe9e
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-20 14:18:48 +0800

    1

--- 04analysis/09site_redirect_chr.py
+++ 04analysis/09site_redirect_chr.py
@@ -1,16 +1,19 @@
 import sys
 import pandas as pd
 
-# fai_name = 'data/cp.chr1B.fa.fai'
-# anno_name = 'data/test.gff3'
-# output_name = 'data/stats.anno.gff3'
+fai_name = 'data/cp.chr1B.fa.fai'
+anno_name = 'data/test.gff3'
+output_name = 'data/stats.anno.gff3'
 
-fai_name = sys.argv[1]
-anno_name = sys.argv[2]
-output_name = sys.argv[3]
+# fai_name = sys.argv[1]
+# anno_name = sys.argv[2]
+# output_name = sys.argv[3]
 
 fai = pd.read_table(fai_name, sep='\t', header=None)
 fai.columns = ['seq', 'length', 'start', 'lb', 'lw']
+
+print(f"fai['seq'][1]: {fai['seq'][1]}")
+print(f"fai['length'][0]: {fai['length'][0]}")
 
 with open(anno_name, 'r') as anno_file, open(output_name, 'w') as output_file:
     for line in anno_file:
@@ -18,7 +21,9 @@ with open(anno_name, 'r') as anno_file, open(output_name, 'w') as output_file:
         chrom = element[0]
         start = int(element[3])
         end = int(element[4])
-        if chrom == fai['seq'][1]:
-            element[3] = str(start + int(fai['end'][1]))
-            element[4] = str(end + int(fai['end'][1]))
+        if chrom == str(fai['seq'][1]):
+            element[3] = str(start + int(fai['length'][0]))
+            element[4] = str(end + int(fai['length'][0]))
+            print(f"element[3]: {element[3]}")
+            print(f"element[4]: {element[4]}")
         output_file.write('\t'.join(element))
