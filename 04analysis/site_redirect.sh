6380daa413dbd040afecffd4a97d21fc51e6f5fc
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-20 14:23:23 +0800

    1

--- 04analysis/site_redirect.sh
+++ 04analysis/site_redirect.sh
@@ -4,7 +4,7 @@ touch stats.anno.gff3
 ## $1: {seq}.fai
 ## $2: {seq}.mod.EDTA.TEanno.gff3
 python ~/script/04analysis/09site_redirect_chr.py $1 $2 stats.anno.gff3
-awk '{$1="chr1B"}1' OFS='\t' stats.anno.gff3 > stats.anno.mod.gff3
+awk '{$1="chr1D"}1' OFS='\t' stats.anno.gff3 > stats.anno.mod.gff3
 
 ## 2. N version
 ## step 1 redirect N_cut annotation
