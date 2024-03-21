bccb384b5c59d5e0a3c06b4bf4e8e6e378458449
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-21 21:56:43 +0800

    1

--- 04analysis/16plot_overlap.py
+++ 04analysis/16plot_overlap.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
-# overlap in both 80%
+# overlap in mean 80%
 total_TE = 150013
-overlap_TE = 21364
-overlap_TE_same_type = 9868
-# overlap in latter 80%
-overlap_TE_latter = 23524
-overlap_TE_same_type_latter =
+
+overlap = pd.read_table('data/overlap.txt', sep='\s+', header=None)
+overlap.columns = ['method', 'overlap', 'same type']
+
+overlap['overlap'] = overlap['overlap'] / total_TE
+overlap['same type'] = overlap['same type'] / total_TE
+
+# plot
+plt.figure(figsize=(12, 8))
+sns.barplot(data=overlap, x='method', y='overlap', label='overlap', width=0.2)
+sns.barplot(data=overlap, x='method', y='same type', label='same type', width=0.2, alpha=0.6)
+plt.title('Number of TEs in 80% overlap')
+plt.legend()
+plt.show()
