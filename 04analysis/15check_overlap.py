dd0f5451914af432caf9bd5954b4669d40405521
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-21 21:05:38 +0800

    1

--- 04analysis/15check_overlap.py
+++ 04analysis/15check_overlap.py
@@ -2,11 +2,11 @@ import sys
 
 import pandas as pd
 
-# anno_name = 'data/test.ln.gff3'
-# output_name = 'data/test.gff3'
+anno_name = 'data/test.ln.gff3'
+output_name = 'data/test.gff3'
 
-anno_name = sys.argv[1]
-output_name = sys.argv[2]
+# anno_name = sys.argv[1]
+# output_name = sys.argv[2]
 
 anno = pd.read_table(anno_name, sep='\t', header=None)
 anno.columns = ['seqid', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'attributes', 'Classification']
@@ -20,7 +20,7 @@ fm_width = 0
 # flexible in 80% of the width
 for i in range(0, len(anno)):
     # if (fm_end - anno['start'][i]) > (fm_width + anno['width'][i]) / 10 and anno['Classification'][i] == fm_class:
-    if (fm_end - anno['start'][i]) > (fm_width + anno['width'][i]) / 10:
+    if (fm_end - anno['start'][i]) > (fm_width + anno['width'][i]) * 0.4:
         anno.loc[i, 'overlap'] = 1
     fm_end = anno['end'][i]
     fm_class = anno['Classification'][i]
