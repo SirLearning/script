922565c4f37133b5d658dd2c786de4871df0b5f9
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-21 16:08:49 +0800

    Update window_dtb.sh

--- 04analysis/window_dtb.sh
+++ 04analysis/window_dtb.sh
@@ -4,8 +4,21 @@
 awk '{print $1"\t"$2}' $2 > stats.genome
 bedtools makewindows -g stats.genome -w $3 -s $3 > stats.dtb.bed3
 awk '{print $1"\t"$2"\t"$3"\t"NR"\t.\t+"}' stats.dtb.bed3 > stats.dtb.bed6
-declare -a arr=("DHH" "DTA" "DTC" "DTH" "DTM" "DTT" "DTX" "DXX" "NULL" "RIJ" "RIL" "RIR" "RIX" "RLC" "RLG" "RLX" "RSX" "XXX")
-for i in "${arr[@]}"
-do
-   awk -v var="$i" '$10=="var"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > "stats.${i}.dtb.txt"
-done
+
+awk -F'\t' '$10=="DHH"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DHH.dtb.txt
+awk -F'\t' '$10=="DTA"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DTA.dtb.txt
+awk -F'\t' '$10=="DTC"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DTC.dtb.txt
+awk -F'\t' '$10=="DTH"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DTH.dtb.txt
+awk -F'\t' '$10=="DTM"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DTM.dtb.txt
+awk -F'\t' '$10=="DTT"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DTT.dtb.txt
+awk -F'\t' '$10=="DTX"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DTX.dtb.txt
+awk -F'\t' '$10=="DXX"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.DXX.dtb.txt
+awk -F'\t' '$10=="RIJ"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RIJ.dtb.txt
+awk -F'\t' '$10=="RIL"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RIL.dtb.txt
+awk -F'\t' '$10=="RIR"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RIR.dtb.txt
+awk -F'\t' '$10=="RIX"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RIX.dtb.txt
+awk -F'\t' '$10=="RLC"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RLC.dtb.txt
+awk -F'\t' '$10=="RLG"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RLG.dtb.txt
+awk -F'\t' '$10=="RLX"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RLX.dtb.txt
+awk -F'\t' '$10=="RSX"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.RSX.dtb.txt
+awk -F'\t' '$10=="XXX"' $1 | bedtools coverage -a stats.dtb.bed6 -b - -counts -F 0.5 > stats.XXX.dtb.txt
