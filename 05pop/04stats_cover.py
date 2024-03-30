7c0409077a59de14e06a005dc46a5e888272d00e
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-03-30 18:39:44 +0800

    2024.3.30

--- 05pop/04stats_cover.py
+++ 05pop/04stats_cover.py
@@ -12,7 +12,7 @@ fai_6A.columns = ['name', 'length', 'offset', 'linebases', 'linewidth']
 
 cover = pd.read_table(cover_name, sep='\s+', header=None)
 cover.columns = ['SW score', 'perc div.', 'perc del.', 'perc ins.', 'Query', 'begin', 'end', '(left)',
-                 'C+', 'Match', 'match_class', 'begin', 'end', 'left', 'ID', 'star']
+                 'C+', 'Match', 'match_class', 'begin', 'end', 'left', 'ID']
 
 cover['query_class'] = cover['Query'].str.split('#').str[1]
 cover['Match'] = cover['Match'] + '#' + cover['match_class']
@@ -21,10 +21,13 @@ cover['cover'] = 'low'
 threshold = 0.8
 
 for i in cover.index:
+    print(fai_6A.loc[fai_1A['name'] == cover.loc[i, 'Query'], 'length'])
     if cover.loc[i, 'query_class'] == cover.loc[i, 'match_class']:
         cover.loc[i, 'class'] = 'same'
-    cover.loc[i, 'query_cover'] = (cover.loc[i, 'end'] - cover.loc[i, 'begin']) / fai_6A.loc[fai_6A.index.isin(cover['Query']), 'length']
-    cover.loc[i, 'match_cover'] = (cover.loc[i, 'end'] - cover.loc[i, 'begin']) / fai_1A.loc[fai_1A.index.isin(cover['Match']), 'length']
+    cover.loc[i, 'query_cover'] = (cover.loc[i, 'end'] - cover.loc[i, 'begin'] + 1) / \
+                                  fai_6A.loc[fai_6A['name'] == cover.loc[i, 'Query'], 'length']
+    cover.loc[i, 'match_cover'] = (cover.loc[i, 'end'] - cover.loc[i, 'begin'] + 1) / \
+                                  fai_1A.loc[fai_1A['name'] == cover.loc[i, 'Match'], 'length']
     if cover.loc[i, 'query_cover'] > threshold and cover.loc[i, 'match_cover'] > threshold:
         cover.loc[i, 'cover'] = 'high'
 
@@ -32,7 +35,9 @@ with open(output_name, 'w') as f:
     f.write('Query\tMatch\tclass\tcover\n')
     for i in cover.index:
         if cover.loc[i, 'class'] == 'same' and cover.loc[i, 'cover'] == 'high':
-            f.write(cover.loc[i, 'Query'] + '\t' + cover.loc[i, 'Match'] + '\t' + cover.loc[i, 'class'] + '\t' + cover.loc[i, 'cover'] + '\n')
+            f.write(
+                cover.loc[i, 'Query'] + '\t' + cover.loc[i, 'Match'] + '\t' + cover.loc[i, 'class'] + '\t' + cover.loc[
+                    i, 'cover'] + '\n')
 
 grouped = cover.groupby(['Query', 'Match', 'class', 'cover']).size()
 print(grouped)
