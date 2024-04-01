f29036f7c949e81af98a442f53ed9b1e0d6d041e
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-01 19:04:35 +0800

    Update 05reads_in_1k.sh

--- 05pop/05reads_in_1k.sh
+++ 05pop/05reads_in_1k.sh
@@ -1,38 +1,30 @@
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file8001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log8001 2>&1 &
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file10001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10  > log10001 2>&1 &
-sleep 16200
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file12001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log12001 2>&1 &
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file14001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log14001 2>&1 &
-sleep 16200
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file16001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log16001 2>&1 &
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file18001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log18001 2>&1 &
-sleep 16200
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file20001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log20001 2>&1 &
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file22001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log22001 2>&1 &
-sleep 16200
-nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file24001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log24001 2>&1 &
+#nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file18001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log18001 2>&1 &
+#nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file20001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log20001 2>&1 &
+#sleep 18000
+#nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file22001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log22001 2>&1 &
+#nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file24001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log24001 2>&1 &
+#sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file26001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log26001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file28001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log28001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file30001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log30001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file32001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log32001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file34001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log34001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file36001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log36001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file38001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log38001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file40001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log40001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file42001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log42001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file44001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log44001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file46001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log46001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file48001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log48001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file50001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log50001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file52001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log52001 2>&1 &
+sleep 18000
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file54001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log54001 2>&1 &
-sleep 16200
 nohup perl ~/transposon/tools/EDTA/EDTA.pl --genome file56001.fasta --curatedlib ~/transposon/ABD/00data/00library/00curatedLib/abd.fa --overwrite 0 --sensitive 1 --threads 10 > log56001 2>&1 &
 
