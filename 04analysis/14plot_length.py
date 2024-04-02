4608a989ab450f2fd3a74a951472071ce092b52d
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-02 19:38:53 +0800

    1

--- 04analysis/14plot_length.py
+++ 04analysis/14plot_length.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 # def plot_length(triticeae):
-#     length = pd.read_table('data/' + triticeae + '/stats.length.txt', sep='\t')
+#     length = pd.read_table('data/' + triticeae + '/stats.length.old.txt', sep='\t')
 #     length.columns = ['Classification', 'width']
 #     length['width (kb)'] = length['width'] / 1000
 #     length['triticeae'] = triticeae  # 添加一个新的列 'triticeae'
@@ -21,27 +21,33 @@ import matplotlib.pyplot as plt
 # plt.title('TE Length Distribution in different annotation')
 # plt.legend(title='Name')  # 添加图例
 # plt.show()
-
+output_name = 'data/stats.length.txt'
 
 def plot_length(name):
     length = pd.read_table('data/' + name + '/stats.length.txt', sep='\t')
     length.columns = ['Classification', 'width']
-    length['width (kb)'] = length['width'] / 1000
+    length['length (kb)'] = length['width'] / 1000
     length['triticeae'] = name  # 添加一个新的列 'triticeae'
     return length
 
 
 fig, ax = plt.subplots()
+plt.rcParams['font.size'] = 24
 ax.figure.set_size_inches(24, 12)
 
 # Concatenate the dataframes
-length = pd.concat([plot_length('CS'), plot_length('EDTA'), plot_length('trep')])
+length = pd.concat([plot_length('CS'), plot_length('EDTA'), plot_length('curated_lib')])
+grouped = length.groupby(['Classification', 'triticeae']).mean()
+print(grouped.to_markdown())
+grouped.to_csv(output_name, sep='\t', header=False)
+
 
 length.reset_index(drop=True, inplace=True)
 
-sns.boxplot(x='Classification', y='width (kb)', hue='triticeae', data=length, showfliers=False,
+sns.boxplot(x='Classification', y='length (kb)', hue='triticeae', data=length, showfliers=False,
             palette='Set3', linewidth=2, dodge=True, ax=ax, width=0.8)
 plt.xticks(rotation=45)
 plt.title('TE Length Distribution in different annotation')
 plt.legend(title='Name')  # 添加图例
+plt.tick_params(axis='both', labelsize=18)
 plt.show()
