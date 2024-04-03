3b209fe84f9916cc388da8d21628f26b67c309c5
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-03 20:36:51 +0800

    Update 06classify_lib_files.py

--- 05pop/06classify_lib_files.py
+++ 05pop/06classify_lib_files.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 lib_name = 'data/intact.fa.fai'
+TElib_name = 'data/TElib.fai'
+intactTE_name = 'data/intactTE.fai'
 
 # Read the library file
 lib = pd.read_table(lib_name, sep="\t", header=None)
@@ -17,5 +19,12 @@ for i in range(0, len(lib)):
 grouped = lib.groupby('type')
 summary = grouped.agg(
     count=('seq', 'count'),
+    mean_length=('length', 'mean'),
+    sum_length=('length', 'sum'),
+    max_length=('length', 'max'),
+    min_length=('length', 'min')
 )
 print(summary)
+
+lib[lib['type'] == 'TElib'].to_csv(TElib_name, sep='\t', index=False)
+lib[lib['type'] == 'intacLib'].to_csv(intactTE_name, sep='\t', index=False)
