3b1626638740506c7ec000ae91508040604e773f
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-08 19:12:07 +0800

    1

--- 05pop/10EDTA_chr_length.py
+++ 05pop/10EDTA_chr_length.py
@@ -6,11 +6,15 @@ anno_name = sys.argv[1]
 tecode_name = sys.argv[2]
 output_name = sys.argv[3]
 
+# anno_name = 'data/test.gff3'
+# tecode_name = 'data/TEcode'
+# output_name = 'data/length.txt'
+
 anno = pd.read_table(anno_name, sep='\t', header=None)
 anno.columns = ['seqid', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'attributes']
 attributes = anno['attributes'].str.split(';', expand=True)
 attributes.columns = ['ID', 'Name', 'Classification', 'Sequence_ontology', 'Identity', 'Method', 'others1',
-                      'others2', 'others3']
+                      'others2']
 anno = pd.concat([anno, attributes], axis=1)
 anno['length'] = anno['end'] - anno['start'] + 1
 
@@ -24,11 +28,11 @@ TEcode = pd.read_table(tecode_name, sep=",", header=None)
 TEcode.columns = ['cls', 'new_cls']
 anno.loc[anno['Classification'] == 'Unspecified', 'Classification'] = anno['Name'].str.split('=').str[1]
 anno.loc[:, 'Classification'] = anno['Classification'].str.split('_').str[0]
-anno['lib_correct'] = 2
+anno['lib_correct'] = False
 for i in range(0, len(TEcode)):
     anno.loc[anno['Classification'] == TEcode['cls'][i], 'Classification'] = TEcode['new_cls'][i]
-    anno.loc[anno['Classification'] == TEcode['new_cls'][i], 'lib_correct'] = 4
-anno.drop(anno['lib_correct'] == 2, axis=0)
+    anno.loc[anno['Classification'] == TEcode['new_cls'][i], 'lib_correct'] = True
+anno = anno[anno['lib_correct'] == True]
 
 te_length = pd.concat([anno['Classification'], anno['length']], axis=1)
 with open(output_name, 'w') as f:
