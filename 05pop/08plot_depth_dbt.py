985c73505cd1ccc83b40a1c4dd61a1e0bfc716b8
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-08 17:12:23 +0800

    2024.4.8

--- 05pop/08plot_depth_dbt.py
+++ 05pop/08plot_depth_dbt.py
@@ -1,14 +1,17 @@
 import collections
 from operator import itemgetter
 import itertools as it
-
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def main():
     args = "data/test.txt"
+    summary_name = "data/test"
     prb = (x.rstrip().split("\t") for x in open(args))
     dtb = collections.defaultdict(list)
+    summary = pd.read_table(summary_name, sep="\t", header=None)
+    summary.columns = ['chrom', 'length', 'bases', 'mean', 'min', 'max']
     for chrom, data in it.groupby(prb, itemgetter(0)):
         data_list = list(data)
         depth, dens = [], []
@@ -18,12 +21,14 @@ def main():
         yb = 0
         found = False
         v50 = 0
-
+        depth.append(xb)
+        dens.append(yb)
+        count = 0
         for _, x, y in data_list:
+            count += 1
             tab = xb - float(x)
-            print(step, tab)
             if tab > step:
-                depth.append(xb)
+                depth.append((xb + float(x)) / 2)
                 xb = float(x)
                 dens.append(float(y) - yb)
                 yb = float(y)
@@ -33,6 +38,9 @@ def main():
                 found = True
         depth.append(0)
         dens.append(0)
+
+        exp = sum([d * p for d, p in zip(dens, depth)])
+        print(exp, count, summary[summary['chrom'] == chrom]['length'].values[0])
 
         dtb[chrom].append({
             'depth': [round(x, 3) for x in depth],
@@ -45,7 +53,9 @@ def main():
         plt.title('Probability Density Plot for ' + chrom)
         plt.xlabel('Depth')
         plt.ylabel('Probability Density')
+        plt.legend([v50])
         plt.show()
+    print([8.01, 31.31, 517.14, 3445.41, 3437.80, 12.20, 17.69])
 
 if __name__ == '__main__':
     main()
---
+++ 05pop/10EDTA_chr_length.py
@@ -0,0 +1,32 @@
+import sys
+
+import pandas as pd
+
+anno_name = sys.argv[1]
+tecode_name = sys.argv[2]
+output_name = sys.argv[3]
+
+anno = pd.read_table(anno_name, sep='\t', header=None)
+anno.columns = ['seqid', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'attributes']
+attributes = anno['attributes'].str.split(';', expand=True)
+attributes.columns = ['ID', 'Name', 'Classification', 'Sequence_ontology', 'Identity', 'Method', 'others1',
+                      'others2', 'others3']
+anno = pd.concat([anno, attributes], axis=1)
+anno['length'] = anno['end'] - anno['start'] + 1
+
+# 1. rearrange classification
+anno['Classification'] = anno['Classification'].str.split('=').str[1]
+# 1.2 no Parent
+index = ~anno['Name'].str.contains('Parent')
+anno = anno[index]
+# 1.3 Unspecified annotation
+TEcode = pd.read_table(tecode_name, sep=",", header=None)
+TEcode.columns = ['cls', 'new_cls']
+anno.loc[anno['Classification'] == 'Unspecified', 'Classification'] = anno['Name'].str.split('=').str[1]
+anno.loc[:, 'Classification'] = anno['Classification'].str.split('_').str[0]
+for i in range(0, len(TEcode)):
+    anno.loc[anno['Classification'] == TEcode['cls'][i], 'Classification'] = TEcode['new_cls'][i]
+
+te_length = pd.concat([anno['Classification'], anno['length']], axis=1)
+with open(output_name, 'w') as f:
+    te_length.to_csv(f, sep='\t', index=False)
