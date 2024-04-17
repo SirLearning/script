ba94a03fbfdf0e0edbcf06b899a5db141cfcedcb
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-17 15:41:42 +0800

    Update 02subsample_reads.sh

--- 05pop/02subsample_reads.sh
+++ 05pop/02subsample_reads.sh
@@ -1,16 +1,30 @@
-java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.2.fq cs.r2.fq.gz r2.2.fq 127
-2 127
-4 253
-6 380
-8 507
-10 633
-12 760
-14 887
-16 1013
-18 1140
-20 1267
-22 1393
-24 1520
-26 1647
-28 1773
-30 1900
+#java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.2.fq cs.r2.fq.gz r2.2.fq 127
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.4.fq cs.r2.fq.gz r2.4.fq 253 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.6.fq cs.r2.fq.gz r2.6.fq 380 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.8.fq cs.r2.fq.gz r2.8.fq 507 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.10.fq cs.r2.fq.gz r2.10.fq 633 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.12.fq cs.r2.fq.gz r2.12.fq 760 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.14.fq cs.r2.fq.gz r2.14.fq 887 &
+#nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.16.fq cs.r2.fq.gz r2.16.fq 1013 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.18.fq cs.r2.fq.gz r2.18.fq 1140 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.20.fq cs.r2.fq.gz r2.20.fq 1267 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.22.fq cs.r2.fq.gz r2.22.fq 1393 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.24.fq cs.r2.fq.gz r2.24.fq 1520 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.26.fq cs.r2.fq.gz r2.26.fq 1647 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.28.fq cs.r2.fq.gz r2.28.fq 1773 &
+nohup java -cp ~/js/Compiler/production/pop ssReads 1 cs.f1.fq.gz f1.30.fq cs.r2.fq.gz r2.30.fq 1900 &
+#2 127
+#4 253
+#6 380
+#8 507
+#10 633
+#12 760
+#14 887
+#16 1013
+#18 1140
+#20 1267
+#22 1393
+#24 1520
+#26 1647
+#28 1773
+#30 1900
