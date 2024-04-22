de69c9c1f00c98dac450de175e5ba0453462db04
sirlearning <zhengnda@outlook.com>
2024-04-22 10:11:03 +0800

    Update 03map_threshold.sh

--- 04ana/pop/03map_threshold.sh
+++ 04ana/pop/03map_threshold.sh
@@ -1,4 +1,4 @@
-# nohup sh ~/scripts/05pop/03map_threshold.sh 2 1A &
+# nohup sh ~/script/04ana/pop/03map_threshold.sh 20 1A &
 bwa-mem2 mem -t 15 $2.known.fa ~/transposon/ABD/00data/03reads/f1.$1.fq.gz ~/transposon/ABD/00data/03reads/r2.$1.fq.gz | samtools view -S -b - > $1.raw.bam
 
 samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && rm $1.raw.bam
