e28850fb43d88b14b394186e31740b355dd1ba0b
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-29 13:03:18 +0800

    1

--- 04ana/pop/03map_threshold.sh
+++ 04ana/pop/03map_threshold.sh
@@ -1,12 +1,19 @@
 # nohup sh ~/script/04ana/pop/03map_threshold.sh 20 1A &
 bwa-mem2 mem -t 15 $2.known.fa ~/transposon/ABD/00data/03reads/f1.$1.fq.gz ~/transposon/ABD/00data/03reads/r2.$1.fq.gz | samtools view -S -b - > $1.raw.bam
 
-samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && rm $1.raw.bam
-samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && rm $1.sort.bam
-samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && rm $1.fixmate.bam
+#samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && rm $1.raw.bam
+#samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && rm $1.sort.bam
+#samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && rm $1.fixmate.bam
+#samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && rm $1.fixmate.pos.bam
+#samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1 $1.rmdup.bam
+#samtools flagstat $1.rmdup.bam > $1.stats.txt
+
+samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && samtools flagstat $1.raw.bam > t1.$1.stats.txt
+samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && samtools flagstat $1.sort.bam > t2.$1.stats.txt
+samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && samtools flagstat $1.fixmate.bam > t3.$1.stats.txt
 samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && rm $1.fixmate.pos.bam
 samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1 $1.rmdup.bam
-samtools flagstat $1.rmdup.bam > $1.stats.txt
+samtools flagstat $1.rmdup.bam > t4.$1.stats.txt
 
 # get mapping rate
 #find . -name "*stats.txt" -exec awk 'FNR==8{print FILENAME ": " $0}' {} \;
