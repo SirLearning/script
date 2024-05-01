b550891e339705059e8af8e3409e44cd0f17373b
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-01 11:43:52 +0800

    Update pop.sh

--- 04ana/pop/pop.sh
+++ 04ana/pop/pop.sh
@@ -1,9 +1,9 @@
 # we
-sh ~/script/04ana/pop/03map_threshold.sh CRR072337 ab.we
-sh ~/script/04ana/pop/03map_threshold.sh CRR072338 ab.we
-sh ~/script/04ana/pop/03map_threshold.sh CRR072340 ab.we
-sh ~/script/04ana/pop/03map_threshold.sh CRR072341 ab.we
-sh ~/script/04ana/pop/03map_threshold.sh CRR072347 ab.we
+sh ~/script/04ana/pop/03map_threshold.sh CRR072337 ab.durum
+sh ~/script/04ana/pop/03map_threshold.sh CRR072338 ab.durum
+sh ~/script/04ana/pop/03map_threshold.sh CRR072340 ab.durum
+sh ~/script/04ana/pop/03map_threshold.sh CRR072341 ab.durum
+sh ~/script/04ana/pop/03map_threshold.sh CRR072347 ab.durum
 
 #cat *lib.fa | grep ">" | grep -v "Unknown" | grep -v "unknown" > list
 #cut -c 2- list > list.cut
