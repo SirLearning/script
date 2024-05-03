727f118549ddcab787ccfa120fe224f9f400133f
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-04 01:57:50 +0800

    1

--- 00basic/bam_file.sh
+++ 00basic/bam_file.sh
@@ -5,4 +5,22 @@ samtools view filename.bam | less -S # view the bam file
 
 prefetch -X 200G -O ./ SRR_ID # download the SRR_ID file
 fasterq-dump --split-3 --defline-seq '@$sn[_$rn]/$ri' --defline-qual '+' SRR_ID # split the SRR_ID file
-pigz
+nohup fasterq-dump -p -e 20 --split-3 -O ./ SRR7164628/SRR7164628.sra
+
+#nohup fasterq-dump -e 20 --split-3 -O ./ SRR7164604/SRR7164604.sra && pigz -3 SRR7164604_1.fastq.gz SRR7164604_1.fastq.gz && rm SRR7164604/SRR7164604.sra &
+#nohup fasterq-dump -e 20 --split-3 -O ./ SRR7164620/SRR7164620.sra && pigz -3 SRR7164620_1.fastq.gz SRR7164620_1.fastq.gz && rm SRR7164620/SRR7164620.sra &
+#nohup fasterq-dump -e 20 --split-3 -O ./ SRR7164628/SRR7164628.sra && pigz -3 SRR7164628_1.fastq SRR7164628_2.fastq && rm SRR7164628/SRR7164628.sra &
+#nohup fasterq-dump -e 20 --split-3 -O ./ SRR7164669/SRR7164669.sra && pigz -3 SRR7164669_1.fastq SRR7164669_2.fastq && rm SRR7164669/SRR7164669.sra &
+#nohup fasterq-dump -e 20 --split-3 -O ./ SRR7164670/SRR7164670.sra && pigz -3 SRR7164670_1.fastq SRR7164670_2.fastq && rm SRR7164670/SRR7164670.sra &
+
+java -cp ~/js/pop/src/ ssReads 1 SRR7164604/SRR7164604_1.fastq.gz f1.SRR7164604.fq SRR7164604/SRR7164604_2.fastq.gz r2.SRR7164604.fq 906 && pigz -3 f1.SRR7164604.fq r2.SRR7164604.fq
+java -cp ~/js/pop/src/ ssReads 1 SRR7164620/SRR7164620_1.fastq.gz f1.SRR7164620.fq SRR7164620/SRR7164620_2.fastq.gz r2.SRR7164620.fq 878 && pigz -3 f1.SRR7164620.fq r2.SRR7164620.fq
+java -cp ~/js/pop/src/ ssReads 1 SRR7164628/SRR7164628_1.fastq.gz f1.SRR7164628.fq SRR7164628/SRR7164628_2.fastq.gz r2.SRR7164628.fq 905 && pigz -3 f1.SRR7164628.fq r2.SRR7164628.fq
+java -cp ~/js/pop/src/ ssReads 1 SRR7164669/SRR7164669_1.fastq.gz f1.SRR7164669.fq SRR7164669/SRR7164669_2.fastq.gz r2.SRR7164669.fq 902 && pigz -3 f1.SRR7164669.fq r2.SRR7164669.fq
+java -cp ~/js/pop/src/ ssReads 1 SRR7164670/SRR7164670_1.fastq.gz f1.SRR7164670.fq SRR7164670/SRR7164670_2.fastq.gz r2.SRR7164670.fq 917 && pigz -3 f1.SRR7164670.fq r2.SRR7164670.fq
+
+10000/11.05259133 =
+10000/11.39205023 =
+10000/11.0573459 =
+10000/11.09383727 =
+10000/10.90905583 =
--- 04ana/pop/pop.sh
+++ 04ana/pop/pop.sh
@@ -11,7 +11,25 @@ sh ~/script/04ana/pop/03map_threshold.sh CRR072347 ab.durum
 #sh ~/script/04ana/pop/03map_threshold.sh CRR072408 d.tauchii
 #sh ~/script/04ana/pop/03map_threshold.sh CRR072409 d.tauchii
 
-#cat *lib.fa | grep ">" | grep -v "Unknown" | grep -v "unknown" > list
-#cut -c 2- list > list.cut
-#rm list && mv list.cut list
-#seqkit grep -f list ab.durum.fa.mod.EDTA.TElib.fa > ab.durum.known.fa
+cat *lib.fa | grep ">" | grep -v "Unknown" | grep -v "unknown" > list
+cut -c 2- list > list.cut
+rm list && mv list.cut list
+seqkit grep -f list ab.durum.fa.mod.EDTA.TElib.fa > ab.durum.known.fa
+
+#nohup sh ~/script/04ana/pop/03map_threshold.sh SRR7164580 1A &
+#nohup sh ~/script/04ana/pop/03map_threshold.sh SRR7164606 1A &
+
+sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 01A a.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 02B b.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 03D d.lancer
+sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 01A a.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 02B b.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 03D d.lancer
+sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 01A a.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 02B b.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 03D d.lancer
+sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 01A a.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 02B b.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 03D d.lancer
+sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 01A a.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 02B b.lancer ; sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 03D d.lancer
+
+
+
+
+
+
+
+
+RepeatMasker -e ncbi -pa 10 -q -no_is -norna -nolow -div 20 -lib $TE2 $TE1 # 80% same
