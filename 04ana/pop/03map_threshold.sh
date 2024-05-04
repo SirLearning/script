c6799788c3a111a00a3f4428c4f99d53f866725a
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-04 11:31:13 +0800

    Update 03map_threshold.sh

--- 04ana/pop/03map_threshold.sh
+++ 04ana/pop/03map_threshold.sh
@@ -8,14 +8,16 @@ bwa-mem2 mem -t 15 $2.known.fa ../f1.$1.fq.gz ../r2.$1.fq.gz | samtools view -S 
 #samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1 $1.rmdup.bam
 #samtools flagstat $1.rmdup.bam >
 
-samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && samtools flagstat $1.raw.bam > t1.$1.stats.txt
-samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && samtools flagstat $1.sort.bam > t2.$1.stats.txt
-samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && samtools flagstat $1.fixmate.bam > t3.$1.stats.txt
-samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && samtools flagstat $1.rmdup.bam > t4.$1.stats.txt
-samtools index $1.raw.bam && mosdepth -t 10 -n -Q 20 $1.raw $1.raw.bam
-samtools index $1.sort.bam && mosdepth -t 10 -n -Q 20 $1.sort $1.sort.bam
-samtools index $1.fixmate.bam && mosdepth -t 10 -n -Q 20 $1.fixmate $1.fixmate.bam
-samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1.rmdup $1.rmdup.bam
+samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && rm $1.raw.bam
+samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && rm $1.sort.bam
+samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && rm $1.fixmate.bam
+samtools flagstat $1.fixmate.pos.bam > stats.$1.txt
+samtools index $1.fixmate.pos.bam && mosdepth -t 10 -n -Q 20 $1 $1.fixmate.pos.bam
+#samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && samtools flagstat $1.rmdup.bam > t4.$1.stats.txt
+#samtools index $1.raw.bam && mosdepth -t 10 -n -Q 20 $1.raw $1.raw.bam
+#samtools index $1.sort.bam && mosdepth -t 10 -n -Q 20 $1.sort $1.sort.bam
+#samtools index $1.fixmate.bam && mosdepth -t 10 -n -Q 20 $1.fixmate $1.fixmate.bam
+#samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1.rmdup $1.rmdup.bam
 
 # get mapping rate
 #find . -name "*stats.txt" -exec awk 'FNR==8{print FILENAME ": " $0}' {} \;
