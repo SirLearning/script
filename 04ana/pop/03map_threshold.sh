ec9a8264fb0ee5fe1d38237943f1314353b0e1c8
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-04 09:50:31 +0800

    q

--- 04ana/pop/03map_threshold.sh
+++ 04ana/pop/03map_threshold.sh
@@ -6,14 +6,16 @@ bwa-mem2 mem -t 15 $2.known.fa ../f1.$1.fq.gz ../r2.$1.fq.gz | samtools view -S 
 #samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && rm $1.fixmate.bam
 #samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && rm $1.fixmate.pos.bam
 #samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1 $1.rmdup.bam
-#samtools flagstat $1.rmdup.bam > $1.stats.txt
+#samtools flagstat $1.rmdup.bam >
 
-samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && samtools flagstat $1.raw.bam > t1.$1.stats.txt && rm $1.raw.bam
-samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && samtools flagstat $1.sort.bam > t2.$1.stats.txt && rm $1.sort.bam
+samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && samtools flagstat $1.raw.bam > t1.$1.stats.txt
+samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && samtools flagstat $1.sort.bam > t2.$1.stats.txt
 samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && samtools flagstat $1.fixmate.bam > t3.$1.stats.txt
-samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && rm $1.fixmate.bam $1.fixmate.pos.bam
-samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1 $1.rmdup.bam
-samtools flagstat $1.rmdup.bam > t4.$1.stats.txt
+samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && samtools flagstat $1.rmdup.bam > t4.$1.stats.txt
+samtools index $1.raw.bam && mosdepth -t 10 -n -Q 20 $1.raw $1.raw.bam
+samtools index $1.sort.bam && mosdepth -t 10 -n -Q 20 $1.sort $1.sort.bam
+samtools index $1.fixmate.bam && mosdepth -t 10 -n -Q 20 $1.fixmate $1.fixmate.bam
+samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1.rmdup $1.rmdup.bam
 
 # get mapping rate
 #find . -name "*stats.txt" -exec awk 'FNR==8{print FILENAME ": " $0}' {} \;
--- 04ana/pop/pop.sh
+++ 04ana/pop/pop.sh
@@ -19,23 +19,23 @@ seqkit grep -f list ab.durum.fa.mod.EDTA.TElib.fa > ab.durum.known.fa
 #nohup sh ~/script/04ana/pop/03map_threshold.sh SRR7164580 1A &
 #nohup sh ~/script/04ana/pop/03map_threshold.sh SRR7164606 1A &
 
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 a.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 a.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 a.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 a.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 a.lancer
-
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 b.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 b.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 b.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 b.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 b.lancer
-
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 d.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 d.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 d.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 d.lancer
-sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 d.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 a.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 a.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 a.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 a.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 a.lancer
+#
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 b.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 b.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 b.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 b.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 b.lancer
+#
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164604 d.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164620 d.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164628 d.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164669 d.lancer
+#sh ~/script/04ana/pop/03map_threshold.sh SRR7164670 d.lancer
 
 
 
