f8557e5c0381a1537db7b5669b59b0208aee591e
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-09 21:52:33 +0800

    Update rm.sh

--- WeaTE/pop/rm.sh
+++ WeaTE/pop/rm.sh
@@ -5,7 +5,7 @@ rm *.out *.cat *.masked *.tbl
 touch $1.bed
 samtools faidx $1.fa
 python ~/script/WeaTE/pop/derived.py $1.fa.fai $1.bed
-bedtools intersect -f 0.8 -r -v -a $1.bed -b $1.fa.out.bed > $1.$2.bed
+bedtools intersect -f 0.8 -r -v -a $1.bed -b $1.fa.out.bed > $1-$2.bed
 rm $1.bed $1.fa.out.bed
 # to r
 
