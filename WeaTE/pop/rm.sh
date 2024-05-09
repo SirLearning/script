4a42e4d187ea1cb55c6690e452b8778c8bb7eca3
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-09 21:44:38 +0800

    Update rm.sh

--- WeaTE/pop/rm.sh
+++ WeaTE/pop/rm.sh
@@ -1,11 +1,11 @@
 # to h
-RepeatMasker -e ncbi -pa 10 -q -no_is -norna -nolow -div 20 -lib cs.fa la.fa # 80% same
-perl ~/transposon/tools/parseRM/RMout_to_bed.pl la.fa.out base0
+RepeatMasker -e ncbi -pa 10 -q -no_is -norna -nolow -div 20 -lib $2.fa $1.fa # 80% same
+perl ~/transposon/tools/parseRM/RMout_to_bed.pl $1.fa.out base0
 rm *.out *.cat *.masked *.tbl
-touch la.bed
-samtools faidx la.fa
-python ~/script/WeaTE/pop/derived.py la.fa.fai la.bed
-bedtools intersect -f 0.8 -r -v -a la.bed -b la.fa.out.bed > la.cs.bed
-rm la.bed la.fa.out.bed
+touch $1.bed
+samtools faidx $1.fa
+python ~/script/WeaTE/pop/derived.py $1.fa.fai $1.bed
+bedtools intersect -f 0.8 -r -v -a $1.bed -b $1.fa.out.bed > $1.$2.bed
+rm $1.bed $1.fa.out.bed
 # to r
 
