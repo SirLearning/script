23d4456fc2b8ebd4cbdfe11da73f9e9f89a90c88
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-10 17:04:13 +0800

    1

---
+++ WeaTE/pop/extract_derived.sh
@@ -0,0 +1,54 @@
+# we
+sh ~/script/WeaTE/pop/abd.sh CRR072247 we ab
+sh ~/script/WeaTE/pop/abd.sh CRR072248 we ab
+sh ~/script/WeaTE/pop/abd.sh CRR072249 we ab
+sh ~/script/WeaTE/pop/abd.sh CRR072250 we ab
+sh ~/script/WeaTE/pop/abd.sh CRR072251 we ab
+
+# dw
+sh ~/script/WeaTE/pop/abd.sh CRR072337 dw ab
+sh ~/script/WeaTE/pop/abd.sh CRR072338 dw ab
+sh ~/script/WeaTE/pop/abd.sh CRR072340 dw ab
+sh ~/script/WeaTE/pop/abd.sh CRR072341 dw ab
+sh ~/script/WeaTE/pop/abd.sh CRR072347 dw ab
+
+# at
+sh ~/script/WeaTE/pop/abd.sh CRR072405 at d
+sh ~/script/WeaTE/pop/abd.sh CRR072406 at d
+sh ~/script/WeaTE/pop/abd.sh CRR072407 at d
+sh ~/script/WeaTE/pop/abd.sh CRR072408 at d
+sh ~/script/WeaTE/pop/abd.sh CRR072409 at d
+
+# cs
+sh ~/script/WeaTE/pop/abd.sh CRR072401 cs a
+sh ~/script/WeaTE/pop/abd.sh CRR072401 cs b
+cat a.CRR072401.sum b.CRR072401.sum > ab.CRR072401.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164576 cs a
+sh ~/script/WeaTE/pop/abd.sh SRR7164576 cs b
+cat a.SRR7164576.sum b.SRR7164576.sum > ab.SRR7164576.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164580 cs a
+sh ~/script/WeaTE/pop/abd.sh SRR7164580 cs b
+cat a.SRR7164580.sum b.SRR7164580.sum > ab.SRR7164580.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164572 cs a
+sh ~/script/WeaTE/pop/abd.sh SRR7164572 cs b
+cat a.SRR7164572.sum b.SRR7164572.sum > ab.SRR7164572.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164606 cs a
+sh ~/script/WeaTE/pop/abd.sh SRR7164606 cs b
+cat a.SRR7164606.sum b.SRR7164606.sum > ab.SRR7164606.sum
+
+# la
+sh ~/script/WeaTE/pop/abd.sh SRR7164620 la a
+sh ~/script/WeaTE/pop/abd.sh SRR7164620 la b
+cat a.SRR7164620.sum b.SRR7164620.sum > ab.SRR7164620.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164628 la a
+sh ~/script/WeaTE/pop/abd.sh SRR7164628 la b
+cat a.SRR7164628.sum b.SRR7164628.sum > ab.SRR7164628.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164670 la a
+sh ~/script/WeaTE/pop/abd.sh SRR7164670 la b
+cat a.SRR7164670.sum b.SRR7164670.sum > ab.SRR7164670.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164669 la a
+sh ~/script/WeaTE/pop/abd.sh SRR7164669 la b
+cat a.SRR7164669.sum b.SRR7164669.sum > ab.SRR7164669.sum
+sh ~/script/WeaTE/pop/abd.sh SRR7164604 la a
+sh ~/script/WeaTE/pop/abd.sh SRR7164604 la b
+cat a.SRR7164604.sum b.SRR7164604.sum > ab.SRR7164604.sum
