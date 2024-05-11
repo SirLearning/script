1f29b8e95e3131a1b593863602d744f29c0f8b09
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-12 01:58:13 +0800

    1

--- WeaTE/pop/abd.sh
+++ WeaTE/pop/abd.sh
@@ -1,1 +1,4 @@
-cat $3/$1.mosdepth.summary.txt | grep -f $3.$2 > $3.$1.sum
+#cat $3/$1.mosdepth.summary.txt | grep -f $3.$2 > $3.$1.sum
+cat /data1/home/dazheng/transposon/pop/03mapping/$3/$1.mosdepth.summary.txt | grep -f $2-0 > $2-0.$1.sum
+cat /data1/home/dazheng/transposon/pop/03mapping/$3/$1.mosdepth.summary.txt | grep -f $2-1 > $2-1.$1.sum
+cat /data1/home/dazheng/transposon/pop/03mapping/$3/$1.mosdepth.summary.txt | grep -f $2-2 > $2-2.$1.sum
