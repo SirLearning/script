6574c34595a36c2cafaa751a2a3f06fbe9618803
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-11 17:10:31 +0800

    1

---
+++ WeaTE/pop/new_te.sh
@@ -0,0 +1,29 @@
+# a
+cat cs-bcs | grep -f cs-dcs > no-bd # 2674
+cat no-bd | grep -f cs-dw | grep -f cs-we > ol-cs # 1456
+cat no-bd | grep -v -f ol-cs > no-bd-cs # 1218
+cat no-bd-cs | grep -f cs-we > ol-dw # 317
+cat no-bd-cs | grep -v -f ol-dw > ol-we # 901
+
+mv ol-cs ol-3
+mv ol-dw ol-2
+mv ol-we ol-1
+
+# b
+cat cs-acs | grep -f cs-dcs > no-ad
+cat no-ad | grep -f cs-dw | grep -f cs-we > ol-cs
+cat no-ad | grep -v -f ol-cs > no-ad-cs
+cat no-ad-cs | grep -f cs-we > ol-dw
+cat no-ad-cs | grep -v -f ol-dw > ol-we
+
+mv ol-cs ol-3
+mv ol-dw ol-2
+mv ol-we ol-1
+
+# d
+cat cs-acs | grep -f cs-bcs > no-ab
+cat no-ab | grep -f cs-at > ol-cs
+cat no-ab | grep -v -f ol-cs > ol-at
+
+mv ol-cs ol-2
+mv ol-at ol-1
