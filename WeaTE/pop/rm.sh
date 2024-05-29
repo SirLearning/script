0653e83577735770e33cb88383adfd4bf1a629cd
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-05-29 23:06:01 +0800

    1

--- WeaTE/pop/rm.sh
+++ WeaTE/pop/rm.sh
@@ -1,11 +1,11 @@
 # get $1-$2
-RepeatMasker -e ncbi -pa 10 -q -no_is -norna -nolow -div 20 -lib $2.fa $1.fa # 80% same
+RepeatMasker -e ncbi -pa 10 -q -no_is -norna -nolow -div 20 -lib $2.fa $1.fa # 80% same in sequence
 perl ~/transposon/tools/parseRM/RMout_to_bed.pl $1.fa.out base0
 rm *.cat *.masked *.tbl *.cat.gz
 touch $1.bed
 samtools faidx $1.fa
 python ~/script/WeaTE/pop/rm.py $1.fa.fai $1.bed
-bedtools intersect -f 0.8 -r -v -a $1.bed -b $1.fa.out.bed > $1.$2.bed
+bedtools intersect -f 0.8 -r -v -a $1.bed -b $1.fa.out.bed > $1.$2.bed # 80% intersect (sequence region)
 cut -f1 $1.$2.bed > $1-$2
 rm $1.bed $1.fa.out.bed $1.$2.bed
 
