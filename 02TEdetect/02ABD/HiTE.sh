969f22288d7b84096229710a9b72b04300a7e039
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-07-20 15:45:21 +0800

    Create HiTE.sh

---
+++ 02TEdetect/02ABD/HiTE.sh
@@ -0,0 +1,20 @@
+#python main.py  --genome ${genome}  --thread ${thread}  --outdir ${output_dir}
+# --genome /data1/home/dazheng/transposon/ABD/00data/05refer/a.cs.fa \
+# --thread 20 \
+# --outdir /data1/home/dazheng/transposon/ABD/09HiTE/01A \
+
+# $1: genome file
+# $2: output directory
+# $3: thread
+python /data1/home/dazheng/transposon/tools/HiTE/main.py \
+ --genome $1 \
+ --outdir $2 \
+ --thread $3 \
+ --domain 1 \
+ --recover 1 \
+ --annotate 1 \
+ --intact_anno 1 \
+ --BM_EDTA 1 \
+ --EDTA_home /data1/home/dazheng/transposon/tools/EDTA \
+ --coverage_threshold 0.8 \
+ --is_wicker 1
