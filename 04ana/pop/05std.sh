38858666476af76d6e61cda6a7d62acf3fa1fce9
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-07-31 21:01:24 +0800

    1

---
+++ 04ana/pop/05std.sh
@@ -0,0 +1,38 @@
+# nohup sh ~/script/04ana/pop/03map_threshold.sh 20 1A &
+bwa-mem2 mem -t 15 pkaba1.fa ../f1.$1.fq.gz ../r2.$1.fq.gz | samtools view -S -b - > $1.raw.bam
+
+#samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && rm $1.raw.bam
+#samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && rm $1.sort.bam
+#samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && rm $1.fixmate.bam
+#samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && rm $1.fixmate.pos.bam
+#samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1 $1.rmdup.bam
+#samtools flagstat $1.rmdup.bam >
+
+samtools sort -n -m 4G -@ 10 -o $1.sort.bam -O bam $1.raw.bam && rm $1.raw.bam
+samtools fixmate -@ 10 -m $1.sort.bam $1.fixmate.bam && rm $1.sort.bam
+samtools sort -m 4G -@ 10 -o $1.fixmate.pos.bam -O bam $1.fixmate.bam && rm $1.fixmate.bam
+samtools flagstat $1.fixmate.pos.bam > stats.$1.txt
+samtools index $1.fixmate.pos.bam && mosdepth -t 10 -n -Q 20 $1 $1.fixmate.pos.bam
+#samtools markdup -@ 10 -r $1.fixmate.pos.bam $1.rmdup.bam && samtools flagstat $1.rmdup.bam > t4.$1.stats.txt
+#samtools index $1.raw.bam && mosdepth -t 10 -n -Q 20 $1.raw $1.raw.bam
+#samtools index $1.sort.bam && mosdepth -t 10 -n -Q 20 $1.sort $1.sort.bam
+#samtools index $1.fixmate.bam && mosdepth -t 10 -n -Q 20 $1.fixmate $1.fixmate.bam
+#samtools index $1.rmdup.bam && mosdepth -t 10 -n -Q 20 $1.rmdup $1.rmdup.bam
+
+# get mapping rate
+#find . -name "*stats.txt" -exec awk 'FNR==8{print FILENAME ": " $0}' {} \;
+
+#mv CRR072247 CRR072248 CRR072249 CRR072250 CRR072251 /data1/home/dazheng/transposon/pop/00data/01WEW
+#mv CRR072337 CRR072338 CRR072340 CRR072341 CRR072347 /data1/home/dazheng/transposon/pop/00data/02DW
+#mv CRR072405 CRR072406 CRR072407 CRR072408 CRR072409 /data1/home/dazheng/transposon/pop/00data/03AT
+
+sh ~/script/04ana/pop/05std.sh 01
+sh ~/script/04ana/pop/05std.sh 04
+sh ~/script/04ana/pop/05std.sh 2
+sh ~/script/04ana/pop/05std.sh 4
+sh ~/script/04ana/pop/05std.sh 6
+sh ~/script/04ana/pop/05std.sh 8
+sh ~/script/04ana/pop/05std.sh 10
+sh ~/script/04ana/pop/05std.sh 20
+sh ~/script/04ana/pop/05std.sh 30
+sh ~/script/04ana/pop/05std.sh 40
---
+++ WeaTE/ref/BM.py
@@ -0,0 +1,17 @@
+import sys
+
+def overlap(anno1, anno2, mod):
+    rpt = []
+
+
+    return rpt
+
+
+def main():
+    anno1 = sys.argv[1]
+    anno2 = sys.argv[2]
+    rpt = overlap(anno1, anno2, 1)
+
+
+if __name__ == '__main__':
+    main()
