8378df29530d8d5f9c631337a817941d15b98675
sirlearning <zhengnda@outlook.com>
2024-08-12 12:56:40 +0800

    Update TEPID.sh

--- 02TEdetect/02ABD/TEPID.sh
+++ 02TEdetect/02ABD/TEPID.sh
@@ -1,2 +1,2 @@
 # 1. tepid-map
-nohup tepid-map -x /data1/home/dazheng/transposon/load/01test/abd.cs.fa  -p 20 -s 2000 -n CRR072247 -1 f1.CRR072247.fq.gz -2 r2.CRR072247.fq.gz &
+nohup tepid-map -x /data1/home/dazheng/transposon/load/02D/d.cs -y /data1/home/dazheng/transposon/ABD/00data/05refer/d.cs -p 20 -s 2000 -n CRR072401 -1 ../00data/02sample/f1.CRR072401.fq.gz -2 ../00data/02sample/r2.CRR072401.fq.gz &
