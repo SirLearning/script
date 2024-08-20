784c04be2e80503da5f28b4ca853df3aa7af8ad8
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-08-20 13:38:41 +0800

    1

--- WeaTE/ref/stats.py
+++ WeaTE/ref/stats.py
@@ -8,7 +8,7 @@ Module to statistically measure the annotation files.
 
 """:parameter
 fai = '{seq}.fai'
-anno = 'mod.edta.gff3'
+anno = 'mod.anno.gff3'
 output1 = 'stats.summ.txt'
 output2 = 'stats.length.txt'
 output3 = 'stats.overlap.gff3'
