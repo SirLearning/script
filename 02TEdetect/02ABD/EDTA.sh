b1e57a50d8930c25ab0fcff5aefd29012624d375
Zheng_Da <zhengnda@outlook.com>
2025-04-12 14:43:25 +0800

    Update EDTA.sh

--- 02TEdetect/02ABD/EDTA.sh
+++ 02TEdetect/02ABD/EDTA.sh
@@ -5,6 +5,7 @@ export LANG="en_US.UTF-8"
 
 ## $1: {seq}
 ## $2: thread
+## $3: curatedLib: abd, ab, a.mono, a.urartu, d, or triticeae.
 
 
 # run EDTA
@@ -12,13 +13,13 @@ export LANG="en_US.UTF-8"
 # perl ~/transposon/tools/EDTA/EDTA.pl --genome $1 --curatedlib ~/transposon/ABD/00data/00library/curated_lib-db_complete_Rel-19.fasta --overwrite 1 --sensitive 1 --stats 1 --evaluate 1 --threads $2 > test.log 2>&1 &
 
 # 继续运行已经断开的程序，从上次断开的地方开始运行
-perl ~/transposon/tools/EDTA/EDTA.pl \
---genome $1 \
---curatedlib ~/transposon/ABD/00data/00library/trep-db_complete_Rel-19.fasta \
---overwrite 0 \
---sensitive 1 \
---anno 1 \
---threads $2 > test.log 2>&1 &
+#perl ~/transposon/tools/EDTA/EDTA.pl \
+#--genome $1 \
+#--curatedlib ~/transposon/ABD/00data/00library/trep-db_complete_Rel-19.fasta \
+#--overwrite 0 \
+#--sensitive 1 \
+#--anno 1 \
+#--threads $2 > test.log 2>&1 &
 
 nohup perl /data/home/dazheng/software/EDTA/EDTA.pl --genome $1 --overwrite 0 --sensitive 1 --anno 1 --threads $2 --curatedlib /data/home/dazheng/transposon/00library/00curatedLib/$3.fa > test.log 2>&1 &
 
